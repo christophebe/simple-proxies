045f4e1c4b588708801afef2fef79b053f51d8ae
christophebe <christophe.lombart@gmail.com>
2014-01-03 21:45:36 +0100

    bug fixes and add new methods

--- proxy.js
+++ proxy.js
@@ -1,11 +1,45 @@
 var fs = require('fs');
+var request = require("request"); 
+var ping = require ("ping");
 
+var defaultGoogleAdress = "http://www.google.com"; 
 var proxyIndex=-1; 
-var proxyList;
+var allProxies = [];
 
 
 var loadProxyFile = function(file) {
-    proxyList = fs.readFileSync(file).toString().split('\n'); 
+    var proxyList = fs.readFileSync(file).toString().split('\n'); // sorry for the sync ;-) 
+    
+    proxyList.forEach(function(proxyString) {
+    	
+		var proxy = proxyString.split(":");
+		if (proxy.length  != 4 && proxy.length  != 2) {
+			if (proxyString != "") // Ignore Empty line			
+			console.log('Incorrect structure for the proxy :' + proxy + '. Each line should match to [host:port:username:password] or [host:port]');		
+	 	}
+	 	else {
+			var result =  {  
+				host :  proxy[0], 
+				port : proxy[1], 
+			};
+   
+			if (proxy.length == 4) {
+				result.userName = proxy[2];
+				result.password = proxy[3];
+			}	
+
+			result.getUrl = function() {
+				if (this.userName && this.password) 
+					return 'http://' + this.userName + ':' + this.password + '@' + this.host + ':' + this.port;
+				else
+					return 'http://' + this.host + ':' + this.port;
+			};  
+
+			allProxies.push(result);  
+		}	
+    }); 
+    
+    
 };
 
 
@@ -13,38 +47,78 @@ var getProxy = function() {
 	
 	proxyIndex++;
 	
-	if (proxyIndex==proxyList.length)
+	if (proxyIndex==(proxyList.length-1))
 	  proxyIndex=0; 
-	 
-	var proxy = proxyList[proxyIndex].split(":");
-	if (proxy.lenght != 4 && proxy.lenght != 2) 
-		throw new Error('Incorrect structure for the proxy file. Each line should match to [host:port:username:password] or [host:port]');
-	 
-	var result =  {  
-		host :  proxy[0], 
-		port : proxy[1], 
-	};
-		   
-	if (proxy.lenght == 4) {
-		result.userName = proxy[2];
-		result.password = proxy[3];
-	}	
 	
-	result.getUrl = function() {
-    	if (userName && password) 
-			return 'http://' + this.userName + ':' + this.password + '@' + this.host + ':' + this.port;
-		else
-			return 'http://' + this.host + ':' + this.port;
-	};
-	
-	
-	return result;    
+	return allProxies[proxyIndex];    
 };
+
+var getProxies = function() {
+	return allProxies;
+}
+
+
+var checkProxies = function (removeInvalidProxies) {
+	
+	allProxies.forEach(function(proxy) {
+		checkProxy(proxy, removeInvalidProxies);
+	}); 
+
+}
+
+var checkProxy = function(proxy, removeInvalidProxies) {
+ 	ping.sys.probe(proxy.host, function(isAlive){
+
+        if (isAlive) {
+        	checkRequest(proxy, removeInvalidProxies);
+        }
+        else {
+        	console.log('host ' + proxy.getUrl() + ' is dead');
+        	var index = allProxies.indexOf(proxy);
+			allProxies.slice(index,1); 
+        }
+    });	
+}
+
+var checkRequest = function(proxy, removeInvalidProxies) {
+	request.get({ 
+				uri:defaultGoogleAdress,
+				proxy : proxy.getUrl()
+			  }, 
+
+			 function (error, response, body) {
+			 
+			 	if (error) 
+					console.log("Proxy: " + proxy.getUrl() + " - Invalid Proxy - bad luck :-( : "  + error);  
+				else 
+				    console.log("Proxy: " + proxy.getUrl() + " - Status : " + response.statusCode );
+				
+				if ((error || response.statusCode != 200) && removeInvalidProxies) {
+				    var index = allProxies.indexOf(proxy);
+				    allProxies.slice(index,1); 
+				}
+				   		    	
+			}
+			
+	);
+}	
+
+var getNumberOfProxies = function() {
+	return allProxies.length
+}
 
 if (fs.existsSync("./proxies.txt"))
    loadProxyFile("./proxies.txt"); 
 else
 	console.log("proxies.txt not found"); 
+	
+checkProxies(true); 	
 
+
+// Exports the public methods
 module.exports.loadProxyFile = loadProxyFile;
 module.exports.getProxy = getProxy;
+module.exports.checkProxies = checkProxies;
+module.exports.checkProxy = checkProxy;
+module.exports.defaultGoogleAdress; 
+module.exports.getNumberOfProxies;
